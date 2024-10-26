3aaa8e77c6418cdc7efd6e2165cb801a1023744c
= <hey@marcello.gg>
2024-10-25 20:45:51 -0400

    include session storage

--- index.js
+++ index.js
@@ -127,7 +127,143 @@ class LocalBin {
   }
 }
 
+class SessionBin {
+  #db = null;
+  #dbName = "bin_db";
+  #dbStore = "session";
+
+  constructor(db = null) {
+    this.#db = db;
+  }
+
+  async setItem(key, value) {
+    await this.#connect();
+
+    return new Promise((resolve, reject) => {
+      const request = this.#db
+        .transaction(this.#dbStore, "readwrite")
+        .objectStore(this.#dbStore)
+        .put({ key, value });
+
+      request.onsuccess = () => {
+        resolve();
+      };
+
+      request.onerror = () => {
+        reject(request.error);
+      };
+    });
+  }
+
+  async getItem(key) {
+    await this.#connect();
+
+    return new Promise((resolve, reject) => {
+      const request = this.#db.transaction(this.#dbStore).objectStore(this.#dbStore).get(key);
+
+      request.onsuccess = () => {
+        resolve(request.result && request.result.value);
+      };
+
+      request.onerror = () => {
+        reject(request.error);
+      };
+    });
+  }
+
+  async removeItem(key) {
+    await this.#connect();
+
+    return new Promise((resolve, reject) => {
+      const request = this.#db
+        .transaction(this.#dbStore, "readwrite")
+        .objectStore(this.#dbStore)
+        .delete(key);
+
+      request.onsuccess = () => {
+        resolve();
+      };
+
+      request.onerror = () => {
+        reject(request.error);
+      };
+    });
+  }
+
+  async clear() {
+    await this.#connect();
+
+    return new Promise((resolve, reject) => {
+      const request = this.#db
+        .transaction(this.#dbStore, "readwrite")
+        .objectStore(this.#dbStore)
+        .clear();
+
+      request.onsuccess = () => {
+        resolve();
+      };
+
+      request.onerror = () => {
+        reject(request.error);
+      };
+    });
+  }
+
+  async dump() {
+    await this.#connect();
+
+    return new Promise((resolve, reject) => {
+      const request = this.#db.transaction(this.#dbStore).objectStore(this.#dbStore).getAll();
+
+      request.onsuccess = () => {
+        resolve(
+          request.result.reduce((acc, { key, value }) => {
+            acc[key] = value;
+            return acc;
+          }, {})
+        );
+      };
+
+      request.onerror = () => {
+        reject(request.error);
+      };
+    });
+  }
+
+  async #connect() {
+    return new Promise((resolve, reject) => {
+      if (this.#db) {
+        resolve();
+      }
+
+      const request = indexedDB.open(this.#dbName);
+
+      request.onsuccess = () => {
+        this.#db = request.result;
+        resolve();
+      };
+
+      request.onerror = () => {
+        this.#db = null;
+        reject(request.error);
+      };
+
+      request.onupgradeneeded = () => {
+        const db = request.result;
+        db.createObjectStore(this.#dbStore, { keyPath: "key" });
+      };
+    });
+  }
+}
+
 const localBin = new LocalBin();
+const sessionBin = new SessionBin();
 
-module.exports = { localBin };
+document.addEventListener("DOMContentLoaded", function () {
+  if (!sessionStorage.getItem("marcellodotgg_bin")) {
+    sessionStorage.setItem("marcellodotgg_bin", "1");
+    sessionBin.clear();
+  }
+});
 
+module.exports = { localBin, sessionBin };
--- types.d.ts
+++ types.d.ts
@@ -1,12 +1,12 @@
-declare module '@marcellodotgg/bin' {
-    interface BinStorage {
-        setItem(key: string, value: any): Promise<void>;
-        getItem(key: string): Promise<any | undefined>;
-        removeItem(key: string): Promise<void>;
-        clear(): Promise<void>;
-        dump(): Promise<Record<string, any>>;
-    }
+declare module "@marcellodotgg/bin" {
+  interface BinStorage {
+    setItem(key: string, value: any): Promise<void>;
+    getItem(key: string): Promise<any | undefined>;
+    removeItem(key: string): Promise<void>;
+    clear(): Promise<void>;
+    dump(): Promise<Record<string, any>>;
+  }
 
-    readonly sessionBin: BinStorage;
-    readonly localBin: BinStorage;
+  const sessionBin: BinStorage;
+  const localBin: BinStorage;
 }
